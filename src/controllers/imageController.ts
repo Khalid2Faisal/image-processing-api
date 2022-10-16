c10a42fe8a7d646c49c77f207a97e185d4944ac5
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2022-10-16 21:41:26 +0200

    added real functionality to imageService and imageController

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,14 +1,23 @@
 import { Request, Response } from "express";
 import imageService from "../services/imageService";
 
-const resize = (req: Request, res: Response) => {
+const resize = async (req: Request, res: Response) => {
   const filename = req.query.filename as string;
   const width = parseInt(req.query.width as string, 10);
   const height = parseInt(req.query.height as string, 10);
 
-  const result = imageService.resizeImage(filename, width, height);
-
-  res.status(200).send(result);
+  try {
+    const imagePath = (await imageService.resizeImage(filename, width, height)) as
+      | string
+      | Error;
+    // check if the image does not exists
+    if (imagePath instanceof Error) {
+      return res.status(404).send({ message: "Image not found" });
+    }
+    return res.status(200).sendFile(imagePath, { root: "./" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 };
 
 export default { resize };
--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,4 +1,20 @@
-const resizeImage = (fileName: string, width: number, height: number): string =>
-  `I am resizing the image with file name ${fileName} to ${width}x${height}`;
+import sharp from "sharp";
+
+const resizeImage = async (
+  fileName: string,
+  width: number,
+  height: number
+): Promise<string> => {
+  try {
+    const resizedImage = await sharp(`assets/full/${fileName}.jpg`);
+    await resizedImage
+      .resize(width, height)
+      .toFile(`assets/thumb/${fileName}-${width}x${height}.jpg`);
+
+    return `assets/thumb/${fileName}-${width}x${height}.jpg`;
+  } catch (error) {
+    return error;
+  }
+};
 
 export default { resizeImage };
